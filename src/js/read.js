2f29fb10a6ecaefc5263a537386dad6485d5eb47
Simon <oneSimon9398@gmail.com>
2016-08-28 19:35:20 +0800

    Initial commit

---
+++ src/js/read.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Simon on 8/28/16.
+ */
+
+
+/*
+
+var $bookItems = $(".book-item");
+var $seletedBookItem = $(".selected-book");
+
+$bookItem.mouseover(function () {
+    console.log("hover on book item!!!");
+    $seletedBookItem.css("display", "block");
+}).mouseout(function () {
+    console.log("hover out book item");
+    $seletedBookItem.css("display", "none");
+});
+
+*/
+
